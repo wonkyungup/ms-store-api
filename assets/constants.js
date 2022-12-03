6d3abefce2f835c452c0a4ce5360503d9c9f83ca
wonkyungup <jack@netsarang.com>
2022-12-03 23:33:08 +0900

    change code

---
+++ assets/constants.js
@@ -0,0 +1,8 @@
+export default class Defs {
+    static STATUS_CODE_SUCCESS = 200
+    static STATUS_CODE_ERROR = 404
+
+    static MS_STORE_PUPPETEER_USER_DATA_DIR = '/dev/null'
+    static MS_STORE_URL = 'https://apps.microsoft.com/store/detail'
+    static MS_STORE_CATCH_ERROR = 'Please check your ID again or try again in a few minutes.'
+}
